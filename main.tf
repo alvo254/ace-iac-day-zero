9521f68bc11ef00ec4b0f61f8ae0bc75cf547594
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-01-19 17:02:52 +0300

    switch from imperative

--- main.tf
+++ main.tf
@@ -81,9 +81,9 @@ resource "aviatrix_segmentation_network_domain" "BU2" {
 
 //Initialy commented out it uncommented in lab 3
 resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
-  domain_name_1 = "BU1"
-  domain_name_2 = "BU2"
+  domain_name_1 = aviatrix_segmentation_network_domain.BU1.domain_name
+  domain_name_2 = aviatrix_segmentation_network_domain.BU2.domain_name
 
   //This is an explicit dependancy
-  depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
+  # depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
 } 
